16210b053ab948608916daa83fec879267c2570e
K0k1 <140357657+Koki-html@users.noreply.github.com>
2026-04-18 22:09:33 -0400

    Dark mode and more!

---
+++ js/darkmode.js
@@ -0,0 +1,26 @@
+
+const html= document.documentElement;
+const darkmode = document.getElementById("Dark-Mode");
+const currentTheme = localStorage.getItem('theme');
+
+
+const setTheme = theme => {
+    html.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+};
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+    setTheme(savedTheme);
+} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    setTheme('dark');
+} else {
+    setTheme('light');
+}
+
+darkmode.addEventListener('click', () => {
+    const current = html.getAttribute('data-theme');
+    const newTheme = current === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+});
