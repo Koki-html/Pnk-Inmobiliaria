1bd31fc575f8dfbd55cca84388d507c22a03b867
K0k1 <140357657+Koki-html@users.noreply.github.com>
2026-06-20 15:54:01 -0400

    Implement login form in login.php
    Added login form with email and password inputs, and a dark mode script.

--- src/Views/login.php
+++ src/Views/login.php
@@ -2,14 +2,31 @@
 require __DIR__ . '/layouts/head.php';
 ?>
 <!-- CSS part -->
+<link rel="stylesheet" href="/css/login.css">
+<head>
 
-<?php
-require __DIR__ . '/layouts/header.php';
-?>
 <!-- Main content of the webpage -->
+<body>
+    <main>
+        <div id="login-container">
+            <h1>Inicio de Sesión</h1>
+            <hr>
+            <div id="credential-div">
+                <section class="credentials-section">
+                    <label for="mail-input"><h2 class="credential-text">Correo Electrónico</h2>
+                    <input id="mail-input" class="credential-input" type="text" name="" id="">
+                </section>
 
+                <section class="credentials-section">
+                    <label for="psswd-input"><h2 class="credential-text">Contraseña</h2></label>
+                    <input id="psswd-input" class="credential-input" type="password" name="" id="">
+                </section>
+                    <button>Iniciar Sesión</button>
+                <br>
+                <a href="/forgot-password">Olvide mi contraseña.</a>
+            </div>
+        </div>
+    </main>
+</body>
 <!-- Footer of the webpage, no change. -->
-
-<?php
-require __DIR__ . '/layouts/footer.php';
-?>
+<script src="js/darkmode.js"></script>
